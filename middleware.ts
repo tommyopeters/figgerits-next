368ac0fe675f340c495c014a2ba5689e8b8286fb
Tommy OPeters <tommyopeters@gmail.com>
2025-02-06 15:54:28 +0100

    Enable Clerk middleware debug mode

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  debug: true
+});
 
 export const config = {
   matcher: [
